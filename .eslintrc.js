7140d0bf7e9ce7292bae3be9e41e5422d2c5d432
yxy <2829122019@qq.com>
2019-10-14 11:14:51 +0800

    cross

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,21 @@
 module.exports = {
-  "root": true,
-  "extends": [
-    'prettier',
-    'eslint:recommended'
-  ],
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module"
+  root: true,
+  extends: ['prettier', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
   },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true,
-    "commonjs": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    commonjs: true
   },
-  "parser": "babel-eslint",
-  "plugins": ['prettier'],
-  "rules": {
-    "prettier/prettier": "error",
-    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'error',
+  parser: 'babel-eslint',
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
