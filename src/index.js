f109a845d6cb64608a1aa4b7f0a396aad9397fd5
yxy <2829122019@qq.com>
2019-10-14 10:46:23 +0800

    222

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import avatar from './assets/avatar.jpg'
 const a = 111
 console.log(a);
 
+
 const app = document.getElementById('app')
 app.innerHTML = `<h1>Hello, word!!!</h1><img src='${avatar}' />`
